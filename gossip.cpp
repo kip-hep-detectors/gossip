fa6898033f0b333752c2e97c9bdf90f7ba7cefba
Patrick <patrick.eckert@kip.uni-heidelberg.de>
2015-11-05 19:29:00 +0100

    added versioning

--- gossip.cpp
+++ gossip.cpp
@@ -1,16 +1,54 @@
 #include "gossipGUI.h"
-
+#include "gossipConfig.h"
+#include <getopt.h>
 #include "TRint.h"
 
 int main(int argc,char** argv)
 {
-  TRint app("sipmMC", 0, 0);
+	//Parse program options
+	const struct option longopts[] =
+	{
+		{ "version", no_argument, 0, 'v'},
+		{ "help", no_argument, 0, 'h'},
+		{ "parfile", required_argument, 0, 's'},
+		{ 0,0,0,0},
+	};
 
-  gossipGUI gui;
-  gui.BuildMainFrame();
-  if(argc == 2) gui.ReadParaFile(argv[1]);
+	int index;
+	int iarg=0;
 
-  app.Run();
+	//turn off getopt error message
+	//opterr=1;
 
-  return 0;
+	const char* parafile = NULL;
+
+	while(iarg != -1)
+	{
+		iarg = getopt_long(argc, argv, "vhp:", longopts, &index);
+		switch (iarg)
+		{
+			case 'h':
+				cout << "Help option not implemented yet =)" << endl;
+				return 1;
+
+			case 'v':
+				cout << "GosSiP version: " << gossip_VERSION_MAJOR << "." << gossip_VERSION_MINOR << endl;
+				return 1;
+
+			case 's':
+				parafile = optarg;
+				break;
+		}
+	}
+
+	//Run GosSiP
+	TRint app("sipmMC", 0, 0);
+
+	gossipGUI gui;
+	gui.BuildMainFrame();
+	if(parafile) gui.ReadParaFile(parafile);
+
+	app.Run();
+
+	return 0;
 }
